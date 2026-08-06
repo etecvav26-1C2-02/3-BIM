839eb1335d2edcacefe659db58a4b094697f4db7
balzaneli <cauabalzaneli81@gmail.com>
2026-08-06 15:28:25 -0300

    add: ex04 e ex05

--- PA - PROGRAMAÇÃO E ALGORITIMOS/python - Valentino e Caua/python-conversion/python/ex04_soma_pares.py
+++ PA - PROGRAMAÇÃO E ALGORITIMOS/python - Valentino e Caua/python-conversion/python/ex04_soma_pares.py
@@ -3,3 +3,27 @@ Autores: Caua Balzaneli, Valentino Hoehne
 Data: 06/08/2026
 Finalidade: Calcula a soma dos números pares de 1 até um número informado.
 """
+
+numero = int(input("Digite um numero inteiro positivo: "))
+contador = 1
+soma = 0
+
+if(numero > 0):
+
+    while (contador <= numero):
+        if(contador % 2 == 0 ):
+            soma = soma + contador
+
+        contador+=1
+
+    print(f"Soma dos pares: {soma} ")
+
+else:
+    print("Numero invalido")
+
+
+
+
+
+
+
--- PA - PROGRAMAÇÃO E ALGORITIMOS/python - Valentino e Caua/python-conversion/python/ex05_tabuada.py
+++ PA - PROGRAMAÇÃO E ALGORITIMOS/python - Valentino e Caua/python-conversion/python/ex05_tabuada.py
@@ -3,3 +3,19 @@ Autores: Caua Balzaneli, Valentino Hoehne
 Data: 06/08/2026
 Finalidade: Lê um número de 1 a 10 e exibe sua tabuada, validando a entrada.
 """
+numero = int(input("Digite um numero de 1 a 10: "))
+contador = 1
+
+
+while(numero < 1 or numero > 10):
+    print("Valor invalido. Digite novamente: ")
+    numero = int(input("Digite um numero de 1 a 10: "))
+
+
+
+while(contador <= 10):
+    print()
+    
+
+
+
