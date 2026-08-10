0eb7da8b61b099fc9f2b8fe2aec6e84fe0954ca3
etecvav26-1C2-02 <etecvavc@gmail.com>
2026-08-10 14:40:24 -0300

    fix: ex05

--- PA - PROGRAMAÇÃO E ALGORITIMOS/python - Valentino e Caua/python-conversion/python/ex05_tabuada.py
+++ PA - PROGRAMAÇÃO E ALGORITIMOS/python - Valentino e Caua/python-conversion/python/ex05_tabuada.py
@@ -6,18 +6,10 @@ Finalidade: Lê um número de 1 a 10 e exibe sua tabuada, validando a entrada.
 numero = int(input("Digite um numero de 1 a 10: "))
 contador = 1
 
-
-while(numero < 1 or numero > 10):
+while numero < 1 or numero > 10:
     print("Valor invalido. Digite novamente: ")
     numero = int(input("Digite um numero de 1 a 10: "))
 
-
-
-while(contador <= 10):
-    print(numero + "x " + contador + "= " + numero * contador)
-    contador+=1
-
-    
-
-
-
+while contador <= 10:
+    print(f"{numero} x {contador} = {numero * contador}")
+    contador += 1
