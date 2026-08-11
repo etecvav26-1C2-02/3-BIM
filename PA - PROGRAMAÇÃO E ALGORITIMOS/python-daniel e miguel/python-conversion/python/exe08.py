14e4622147487e7a468b1b5a9063ccadd9f755db
Daniel de Sousa Araujo <danielsaraujo175@gmail.com>
2026-08-11 14:25:15 -0300

    Add prime number checker in exe08.py
    This script checks if a positive integer is prime by counting its divisors.

---
+++ PA - PROGRAMAÇÃO E ALGORITIMOS/python-daniel e miguel/python-conversion/python/exe08.py
@@ -0,0 +1,20 @@
+"""
+    Autor: Daniel de sousa Araujo,MIguel Marcelo
+    Data: Junho/2026
+    Descrição: Verifica se um número inteiro positivo é primo.
+"""
+numero = int(input("Digite uma numero inteiro positivo: "))
+divisor= 1
+quantidadeDivisores = 0
+
+if numero > 0:
+    while divisor <= numero:
+        if numero % divisor == 0:
+            quantidadeDivisores += 1
+        divisor += 1
+    if quantidadeDivisores == 2:
+      print("O número é primo.")
+    else:
+      print("O número não é primo.")
+else:
+    print("O número não é primo.(invalido)")
