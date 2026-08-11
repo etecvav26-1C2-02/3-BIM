32cf634751c87a656d678fa7b1b58faeef6d67bd
Daniel de Sousa Araujo <danielsaraujo175@gmail.com>
2026-08-11 14:22:09 -0300

    Add script to calculate valid average grades

---
+++ PA - PROGRAMAÇÃO E ALGORITIMOS/python-daniel e miguel/python-conversion/python/exe06.py
@@ -0,0 +1,18 @@
+"""
+    Auor: Daniel de Sousa Araujo, Miguel marcelo 
+    Data: Agosto/2026
+    Descrição: Calcula a média de notas válidas entre 0 e 10 usando while.
+"""
+nota = float(input("Digite uma nota entre 0 e 10: "))
+quantidade = 0
+soma = 0
+
+while nota > 0 and nota <= 10:
+    soma = soma + nota
+    quantidade += 1
+    nota = float(input("Digite outra nota ou um número fora de 0-10 para sair: "))
+    
+if quantidade > 0: 
+    print("Média das notas: ", soma / quantidade)
+else:
+    print("Nenhuma nota válida foi digitada.")
