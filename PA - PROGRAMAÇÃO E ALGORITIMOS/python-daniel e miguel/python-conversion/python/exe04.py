022015e55dfde058fe6b9676e104a2ef2ed2dd9f
Daniel de Sousa Araujo <danielsaraujo175@gmail.com>
2026-08-11 14:17:23 -0300

    Add program to sum even numbers up to a given integer
    Implementa um programa que lê um número inteiro positivo e calcula a soma dos números pares até esse número.

---
+++ PA - PROGRAMAÇÃO E ALGORITIMOS/python-daniel e miguel/python-conversion/python/exe04.py
@@ -0,0 +1,19 @@
+"""
+    Auor: Daniel de Sousa Araujo, Miguel marcelo 
+    Data: Agosto/2026
+    Descrição: Lê dois números inteiros e informa qual é o maior ou se são iguais.
+"""
+numero = int(input ("Digite um numero inteiro positivo: "))
+i = 1
+soma = 0
+
+
+if numero > 0:
+    while i <= numero:
+        if i % 2 == 0:
+            soma = soma + i 
+        i += 1
+    print ("Soma dos pares é: ", soma )          
+
+else:
+    print("Numero invalido. Digite um valor positivo.")
